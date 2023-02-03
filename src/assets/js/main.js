ada395663e6622078645ae4090c958cc8a3d9710
Miguel Emmanuel <sombrass46@gmail.com>
2023-02-02 20:16:47 -0600

    services section add

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -30,15 +30,40 @@ $( document ).ready(function() {
      $("#cc3").hover(function() {$(".back").css({"display":"block","transform":"rotateY(360deg)","background-color":"#959f3e"}).delay( 800 ); });
      $("#cc4").hover(function() {$(".back").css({"display":"block","transform":"rotateY(360deg)","background-color":"#959f3e"}).delay( 800 ); });
      $("#cc5").hover(function() {$(".back").css({"display":"block","transform":"rotateY(360deg)","background-color":"#959f3e"}).delay( 800 ); });*/
-  
+  /*
 
   $("#cc1").hover(function() {$("#ccb1").css({"display":"block","transform":"rotateY(360deg)","background-color":"#959f3e","transition":"all 1s"});});
 
      $("#cc2").hover(function() {$("#ccb2").css({"display":"block","transform":"rotateY(360deg)","background-color":"#959f3e"}).delay( 800 ); });
      $("#cc3").hover(function() {$("#ccb3").css({"display":"block","transform":"rotateY(360deg)","background-color":"#959f3e"}).delay( 800 ); });
      $("#cc4").hover(function() {$("#ccb4").css({"display":"block","transform":"rotateY(360deg)","background-color":"#959f3e"}).delay( 800 ); });
-     $("#cc5").hover(function() {$("#ccb5").css({"display":"block","transform":"rotateY(360deg)","background-color":"#959f3e"}).delay( 800 ); });
+     $("#cc5").hover(function() {$("#ccb5").css({"display":"block","transform":"rotateY(360deg)","background-color":"#959f3e"}).delay( 800 ); });*/
+
+
+    $(window).scroll(function(){
+        var barra = $(window).scrollTop();
+        var posicion = barra * 0.40;
+
+        $('body').css({
+          'background-position': '0 ' + posicion + 'px'
+        });
+      });
+
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+
+
 
 });
 
+
+
+/*const menuLinks = document.querySelectorAll('.navbar-nav a[href^="#"]');
+menuLinks.foreach(menuLink => {
+  menuLink.addEventListener("click", function(){
+    menu.classList.remove("");
+  });
+});
+*/
 
